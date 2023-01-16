3d220abc3f3786a81c3a72ae2c47f50f44c25599
Fabiola Contreras <98127191+Fabiola-cc@users.noreply.github.com>
2023-01-16 06:57:17 -0600

    Cambios en clase radio

--- Radio.java
+++ Radio.java
@@ -1,16 +1,19 @@
-public class Radio implements IRadio{
+public class Radio implements IRadio {
 
-    private boolean turnOn; 
-    public String frequence; 
-    
+    private boolean turnOn;
+    public String frequence;
+    private Double[] FMStations = new Double[12];
+    private int[] AMStations = new int[12];
+    private int AM_actualStation;
+
     @Override
     public void on() {
-        turnOn = true; 
+        turnOn = true;
     }
 
     @Override
     public void off() {
-        turnOn = false; 
+        turnOn = false;
     }
 
     @Override
@@ -20,14 +23,14 @@ public class Radio implements IRadio{
 
     @Override
     public void setFrequence(String freq) throws Exception {
-        
-        if (freq.equalsIgnoreCase("AM") || freq.equalsIgnoreCase("FM")){
-            frequence = freq; 
 
-        } else { 
-            throw new Exception("¡Por favor solamente seleccione entre AM o FM!"); 
+        if (freq.equalsIgnoreCase("AM") || freq.equalsIgnoreCase("FM")) {
+            frequence = freq;
+
+        } else {
+            throw new Exception("¡Por favor solamente seleccione entre AM o FM!");
         }
-    
+
     }
 
     @Override
@@ -37,14 +40,12 @@ public class Radio implements IRadio{
 
     @Override
     public void Forward() {
-  
-        
+
     }
 
     @Override
     public void Backward() {
 
-        
     }
 
     @Override
@@ -62,38 +63,34 @@ public class Radio implements IRadio{
     @Override
     public void setFMActualStation(double actualStation) {
 
-        
     }
 
     @Override
     public void setAMActualStation(int actualStation) {
+        this.AM_actualStation = actualStation;
 
-        
     }
 
     @Override
     public void saveFMStation(double actualStation, int slot) {
-    
-        
+        FMStations[slot] = actualStation;
+
     }
 
     @Override
     public void saveAMStation(int actualStation, int slot) {
-        
-        
+        AMStations[slot] = actualStation;
     }
 
     @Override
     public double getFMSlot(int slot) {
-      
-        return 0;
+        Double fmSlot = FMStations[slot];
+        return fmSlot;
     }
 
     @Override
     public int getAMSlot(int slot) {
-       
-        return 0;
+        int amSlot = AMStations[slot];
+        return amSlot;
     }
-
-    
 }
