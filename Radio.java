4bd16fcd4695a765af7bd57fb94a7b0953844af2
Nahomy Castro <ncastrom21@gmail.com>
2023-01-17 18:01:16 -0600

    Se agregó Java Doc

--- Radio.java
+++ Radio.java
@@ -1,32 +1,51 @@
 public class Radio implements IRadio {
 
+ 
     private boolean turnOn;
-    public String frequence;
+    public String Frequence;
     private Double[] FMStations = new Double[12];
     private Integer[] AMStations = new Integer[12];
     private int AM_actualStation;
     private double FM_actualStation;
 
+    
+    /* 
+     * Enciende la radio, ingresa a la variable un booleano "True"
+     */
     @Override
     public void on() {
         turnOn = true;
+        setAMActualStation(530);
+        setFMActualStation(87.9);
     }
 
+    /* 
+     * Apaga la radio, ingresa a la variable un booleano "False" 
+     */
     @Override
     public void off() {
         turnOn = false;
     }
 
+    /* 
+     * Verifica si la radio se encuentra encendida. 
+     * @return la variable booleana turn on. (True = Encendida ; False = Apagada)
+     */
     @Override
     public boolean isOn() {
         return turnOn;
     }
 
+    /* 
+     * Establece la frecuencia de la radio 
+     * @param freq La frecuencia la cual varia entre AM y FM 
+     * @throws: Cuando no se ingresa AM o FM, lanza un mensaje de error. 
+     */
     @Override
     public void setFrequence(String freq) throws Exception {
 
         if (freq.equalsIgnoreCase("AM") || freq.equalsIgnoreCase("FM")) {
-            frequence = freq;
+            Frequence = freq;
 
         } else {
             throw new Exception("¡Por favor solamente seleccione entre AM o FM!");
@@ -34,97 +53,119 @@ public class Radio implements IRadio {
 
     }
 
+    /* 
+     * Obtener la frecuencia en la que se encuentra la radio (AM o FM)
+     * @return AM o FM
+     */
     @Override
     public String getFrequence() {
-        return frequence;
+        return Frequence;
     }
 
-    // AM frequencies can be represented by multiples of 10 in the range 530 to
-    // 1610.
-    // FM frequencies are found at multiples of 0.2 in the range 87.9 to 107.9.”
-
-    private void Checkeo() {
-        if (frequence.equalsIgnoreCase("AM")) {
-            setAMActualStation(530);
-        } else if (frequence.equalsIgnoreCase("FM")) {
-            setFMActualStation(87.9);
-        }
-    }
-
+    /* 
+     *Avanza entre las estaciones de radio dependiendo de su frecuencia 
+     */
     @Override
     public void Forward() {
-        Checkeo();
-        if (frequence.equals("AM")) {
+        if (Frequence.equals("AM")) {
             setAMActualStation(AM_actualStation + 10);
-            if (AM_actualStation == 1610) {
+            if (AM_actualStation > 1610) {
                 setAMActualStation(530);
             }
-            System.out.println("Radio actual:" + AM_actualStation);
         }
-        if (frequence.equals("FM")) {
+        if (Frequence.equals("FM")) {
             setFMActualStation(FM_actualStation + 0.2);
-            if (FM_actualStation == 107.9) {
+            if (FM_actualStation > 107.9) {
                 setFMActualStation(87.9);
             }
-            System.out.println("Radio actual:" + FM_actualStation);
         }
 
     }
 
+    /* 
+     * Retrocede entre las estaciones de radio dependiendo de su frecuencia 
+     */
     @Override
     public void Backward() {
-        Checkeo();
-        if (frequence.equals("AM")) {
+        if (Frequence.equals("AM")) {
             setAMActualStation(AM_actualStation - 10);
-            if (AM_actualStation == 1610) {
-                setAMActualStation(530);
+            if (AM_actualStation < 530) {
+                setAMActualStation(1610);
             }
-            System.out.println("Radio actual:" + AM_actualStation);
         }
-        if (frequence.equals("FM")) {
+        if (Frequence.equals("FM")) {
             setFMActualStation(FM_actualStation - 0.2);
-            if (FM_actualStation == 107.9) {
-                setFMActualStation(87.9);
+            if (FM_actualStation < 87.9) {
+                setFMActualStation(107.9);
             }
-            System.out.println("Radio actual:" + FM_actualStation);
         }
 
     }
 
+    /* 
+     * Obtiene la estacion de frecuencia FM actual 
+     * @return Estacion actual 
+     */
     @Override
     public double getFMActualStation() {
         return FM_actualStation;
     }
 
+    /* 
+     * Obtiene la estacion de frecuencia AM actual 
+     * @return Estacion actual 
+     */
     @Override
     public int getAMActualStation() {
         return AM_actualStation;
     }
 
+    /* 
+     * Establecer estacion en la frecuencia FM
+     * @param actualStation Estacion actual 
+     */
     @Override
     public void setFMActualStation(double actualStation) {
         FM_actualStation = actualStation;
 
     }
-
-    // Naho
+    
+    /* 
+     * Establecer estacion en la frecuencia AM
+     * @param actualStation Estacion actual 
+     */
     @Override
     public void setAMActualStation(int actualStation) {
         AM_actualStation = actualStation;
 
     }
 
+    /* 
+     * Guardar una estacion en frecuencia FM
+     * @param actualStation Estacion actual
+     * @param slot Espacio disponible dentro de lista  
+     */
     @Override
     public void saveFMStation(double actualStation, int slot) {
         FMStations[slot - 1] = actualStation;
 
     }
 
+    /* 
+     * Guardar una estacion en frecuencia AM
+     * @param actualStation Estacion actual
+     * @param slot Espacio disponible dentro de lista  
+     */
     @Override
     public void saveAMStation(int actualStation, int slot) {
         AMStations[slot - 1] = actualStation;
     }
 
+    /* 
+     * Obtiene el espacio disponible de las listas de frecuencia FM
+     * @param slot Espacio  
+     * @return Espacio disponible 
+     */
     @Override
     public double getFMSlot(int slot) {
         Double fmSlot = 87.9;
@@ -135,6 +176,11 @@ public class Radio implements IRadio {
         return fmSlot;
     }
 
+    /* 
+     * Obtiene el espacio disponible de las listas de frecuencia AM
+     * @param slot Espacio  
+     * @return Espacio disponible 
+     */
     @Override
     public int getAMSlot(int slot) {
         int amSlot = 530;
--- UI.java
+++ UI.java
@@ -2,15 +2,51 @@ import java.util.Scanner;
 
 public class UI {
 
+    /**
+     * Inicia el programa 
+     * @param args
+     * @throws Exception Muestra mensaje de error al no seleccionar alguna de las dos opciones 
+     */
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
         Radio radio = new Radio();
 
-        menuPrincipal(radio, sc);
-
+        int p = Amenu(radio,sc);
+        if (p==1){
+            System.out.println("\nLa radio ha sido encendida\n");
+            radio.on();
+            menuPrincipal(radio, sc);
+        }
+        else if (p==2){
+            System.out.println("Saliendo...");
+        }
     }
 
+    /**
+     * Menu incial para encender la radio antes de utilizar los metodos 
+     * @param radio Radio instanciada  
+     * @param sc Scanner 
+     * @return Opcion seleccionada por usuario 
+     */
+    public static int Amenu(Radio radio, Scanner sc){
+        int opc;
+        System.out.println("\nLa radio se encuentra: " + radio.isOn() + "\n");
+        System.out.println("¡Bienvenid@! Por favor, seleccione una opción.");
+        System.out.println("");
+        System.out.println("\t1. Encender radio");
+        System.out.println("\t2. Apagar radio");
+
+        opc = sc.nextInt();
+        return opc;
+    }
+
+    /**
+     * Menu con las opciones de la radio 
+     * @param radio Radio instanciada  
+     * @param sc Scanner 
+     * @throws Exception Error al seleccionar opciones fuera de rango 
+     */
     public static void menuPrincipal(Radio radio, Scanner sc) throws Exception {
 
         boolean salir = false;
@@ -18,44 +54,40 @@ public class UI {
         String freq;
 
         while (!salir) {
-
-            System.out.println("\nLa radio se encuentra: " + radio.isOn() + "\n");
-            System.out.println("¡Bienvenid@! Por favor, seleccione una opción.");
-            System.out.println("");
-            System.out.println("\t1. Encender radio");
-            System.out.println("\t2. Seleccionar AM o FM");
-            System.out.println("\t3. Cambiar emisora");
-            System.out.println("\t4. Guardar emisora");
-            System.out.println("\t5. Seleccionar emisora guardada");
-            System.out.println("\t6. Salir");
+            System.out.println("\t1. Seleccionar AM o FM");
+            System.out.println("\t2. Cambiar emisora");
+            System.out.println("\t3. Guardar emisora");
+            System.out.println("\t4. Seleccionar emisora guardada");
+            System.out.println("\t5. Apagar Radio");
             System.out.println("");
             op = sc.nextInt();
 
             switch (op) {
                 case 1:
-                    System.out.println("\nLa radio ha sido encendida\n");
-                    radio.on();
-                    break;
-
-                case 2:
                     System.out.println("¿Qué frecuencia desea escuchar? Escriba AM o FM");
                     freq = sc.next();
                     radio.setFrequence(freq);
                     System.out.println("\tLa emisora se encuentra en: " + radio.getFrequence());
                     break;
                 // Si la persona quiere ir adelante o atras en las emisoras
-                case 3:
-                    System.out.println("Para donde deseas ir? Adelante (1), Atras(2)");
+                case 2:
+                    System.out.println("¿Para donde quieres ir? Adelante (1), Atras(2)");
                     int opcion = sc.nextInt();
                     if (opcion == 1) {
                         radio.Forward();
                     } else if (opcion == 2) {
                         radio.Backward();
                     }
+                    if (radio.Frequence.equals("AM")){
+                        System.out.println("Radio actual: " + radio.getAMActualStation());
+                    }
+                    else if(radio.Frequence.equals("FM")){
+                        System.out.println("Radio actual: " + radio.getFMActualStation());
+                    }
 
                     break;
 
-                case 4: // Guardar emisora
+                case 3: // Guardar emisora
                     System.out.println("¿Qué emisora deseas guardar?");
                     Boolean correcto = true;
                     int emisoraAM = 0;
@@ -67,6 +99,22 @@ public class UI {
                                 System.out.println("Cuidado escribe una emisora entre la 530 y 1610");
                                 correcto = false;
                             }
+                            System.out.println("Muy bien, ¿En qué espacio deseas guardarla?\nEscribe un número del 1 al 12");
+                            while (correcto) {
+                                int slot = sc.nextInt();
+                                if (slot < 1 || slot > 12) {
+                                    System.out.println("Cuidado escribe un slot entre la 1 y 12");
+                                    correcto = false;
+                                } else {
+                                    if (radio.getFrequence().equals("AM")) {
+                                        radio.saveAMStation(emisoraAM, slot);
+                                        correcto = false;
+                                    } else if (radio.getFrequence().equalsIgnoreCase("FM")) {
+                                        radio.saveFMStation(emisoraFM, slot);
+                                        correcto = false;
+                                    }
+                                }
+                            }
                         }
                         if (radio.getFrequence().equalsIgnoreCase("FM")) {
                             emisoraFM = sc.nextDouble();
@@ -74,30 +122,33 @@ public class UI {
                                 System.out.println("Cuidado escribe una emisora entre la 87.9 y 107.9");
                                 correcto = false;
                             }
-                        }
-                    }
+                            System.out.println("Muy bien, ¿En qué espacio deseas guardarla?\nEscribe un número del 1 al 12");
+                            while (correcto) {
+                                int slot = sc.nextInt();
+                                if (slot < 1 || slot > 12) {
+                                    System.out.println("Cuidado escribe un slot entre la 1 y 12");
+                                    correcto = false;
+                                } else {
+                                    if (radio.getFrequence().equalsIgnoreCase("AM")) {
+                                        radio.saveAMStation(emisoraAM, slot);
+                                        correcto = false;
 
-                    System.out.println("Muy bien, ¿en qué espacio deseas guardarla?\nEscribe un número del 1 al 12");
-                    while (correcto) {
-                        int slot = sc.nextInt();
-                        if (slot < 1 || slot > 12) {
-                            System.out.println("Cuidado escribe un slot entre la 1 y 12");
-                            correcto = false;
-                        } else {
-                            if (radio.getFrequence().equalsIgnoreCase("AM")) {
-                                radio.saveAMStation(emisoraAM, slot);
-                            } else if (radio.getFrequence().equalsIgnoreCase("FM")) {
-                                radio.saveFMStation(emisoraFM, slot);
+                                    } else if (radio.getFrequence().equalsIgnoreCase("FM")) {
+                                        radio.saveFMStation(emisoraFM, slot);
+                                        correcto = false;
+
+                                    }
+                                }
                             }
                         }
                     }
 
                     break;
 
-                case 5: // Seleccionar emisora guardada
+                case 4: // Seleccionar emisora guardada
                     System.out.println("Estas en frecuencia " + radio.getFrequence());
                     System.out.println(
-                            "De las emisoras guardadas, ¿cuál deseas seleccionar?\nEscribe el número de slot que deseas");
+                            "De las emisoras guardadas, ¿Cuál deseas seleccionar?\nEscribe el número de slot que deseas");
                     int slot = sc.nextInt();
                     if (slot < 1 || slot > 12) {
                         System.out.println("Cuidado escribe un slot entre la 1 y 12");
@@ -113,7 +164,7 @@ public class UI {
                     }
                     break;
 
-                case 6:
+                case 5:
                     salir = true;
                     System.out.println("\nSaliendo del programa...");
                     break;
